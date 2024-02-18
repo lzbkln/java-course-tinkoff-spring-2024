59205257126255f9e5de87ec789aef75ddd5b9b1
lzbkl <lzbkln@mail.ru>
2024-02-18 20:52:36 +0500

    add link handler test

---
+++ bot/src/test/java/edu/java/bot/handlers/LinkHandlerTest.java
@@ -0,0 +1,49 @@
+package edu.java.bot.handlers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LinkHandlerTest {
+
+    @Test
+    @DisplayName("Test GitHub link handler with valid link")
+    public void testGithubLinkHandlerWithValidLink() {
+        LinkHandler githubLinkHandler = new GithubLinkHandler();
+
+        boolean isValid = githubLinkHandler.isValid("https://github.com/user/repository");
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    @DisplayName("Test GitHub link handler with not valid link")
+    public void testGithubLinkHandlerWithNotValidLink() {
+        LinkHandler githubLinkHandler = new GithubLinkHandler();
+
+        boolean isValid = githubLinkHandler.isValid("https://github.ru/user/repository");
+
+        assertFalse(isValid);
+    }
+
+    @Test
+    @DisplayName("Test StackOverflow link handler with valid link")
+    public void testStackOverflowLinkHandlerWithValidLink() {
+        LinkHandler stackOverflowLinkHandler = new StackOverflowLinkHandler();
+
+        boolean isValid = stackOverflowLinkHandler.isValid("https://stackoverflow.com/questions");
+
+        assertTrue(isValid);
+    }
+
+    @Test
+    @DisplayName("Test Stack Overflow link handler with invalid link")
+    public void testStackOverflowLinkHandlerWithInvalidLink() {
+        LinkHandler stackOverflowLinkHandler = new StackOverflowLinkHandler();
+
+        boolean isValid = stackOverflowLinkHandler.isValid("https://stakoverflow.com/questions");
+
+        assertFalse(isValid);
+    }
+}
