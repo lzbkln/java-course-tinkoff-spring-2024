3d5115537ea706ad741e931a4a3ad47c5b037d8d
lzbkl <lzbkln@mail.ru>
2024-02-18 18:21:05 +0500

    add link handlers to commands

--- bot/src/main/java/edu/java/bot/commands/TrackCommand.java
+++ bot/src/main/java/edu/java/bot/commands/TrackCommand.java
@@ -3,6 +3,7 @@ package edu.java.bot.commands;
 import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import edu.java.bot.entities.User;
+import edu.java.bot.handlers.LinkHandler;
 import edu.java.bot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Component;
 @Component("/track")
 @RequiredArgsConstructor
 public class TrackCommand implements Command {
-    public final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final LinkHandler linkHandler;
     private static final String INVALID_FORMAT_MESSAGE = "Invalid format. Please use: /track <link>";
     private static final String REGISTER_MESSAGE = "Please, register with /start";
     private static final String ALREADY_TRACKED_MESSAGE = "The link is being tracked";
     private static final String COMMAND = "/track";
     private static final String DESCRIPTION_OF_COMMAND = "Start link tracking";
+    private static final String NOT_VALID_LINK = "The link is not valid";
 
     @Override
     public String execute(Update update) {
@@ -40,19 +43,34 @@ public class TrackCommand implements Command {
     }
 
     private String trackCommand(Long id, String[] parts) {
-        if (parts.length != 2) {
-            return INVALID_FORMAT_MESSAGE;
-        }
-        String link = parts[1];
         User user = userRepository.findById(id);
         if (user == null) {
             return REGISTER_MESSAGE;
         }
+
+        if (parts.length != 2) {
+            return INVALID_FORMAT_MESSAGE;
+        }
+
+        String link = parts[1];
+        return processLink(user, link);
+    }
+
+    private String processLink(User user, String link) {
+        if (!linkHandler.isValid(link)) {
+            return NOT_VALID_LINK;
+        }
+
         if (user.getLinks().contains(link)) {
             return ALREADY_TRACKED_MESSAGE;
         }
-        user.getLinks().add(link);
-        userRepository.updateUserById(id, user);
+
+        addLinkAndUpdateUser(user, link);
         return DESCRIPTION_OF_COMMAND;
+    }
+
+    private void addLinkAndUpdateUser(User user, String link) {
+        user.getLinks().add(link);
+        userRepository.updateUserById(user.getId(), user);
     }
 }
