787f610587f5859eece12e7c791fbb46f527caf4
lzbkl <lzbkln@mail.ru>
2024-02-18 23:34:13 +0500

    add User Repository Test

---
+++ bot/src/test/java/edu/java/bot/repository/UserRepositoryTest.java
@@ -0,0 +1,57 @@
+package edu.java.bot.repository;
+
+import edu.java.bot.BotApplication;
+import edu.java.bot.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest(classes = {BotApplication.class})
+public class UserRepositoryTest {
+    private UserRepository userRepository;
+    @Mock
+    private User mockUser;
+    @Mock
+    private User updatedUser;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository = new UserRepository();
+    }
+
+    @Test
+    @DisplayName("Test saving a user")
+    public void testSaveUser() {
+        when(mockUser.getId()).thenReturn(1L);
+        userRepository.saveUser(mockUser);
+
+        assertEquals(1, userRepository.dbUsers.size());
+        assertEquals(mockUser, userRepository.dbUsers.get(1L));
+    }
+
+    @Test
+    @DisplayName("Test finding a user by ID")
+    public void testFindUserById() {
+        when(mockUser.getId()).thenReturn(1L);
+        userRepository.dbUsers.put(1L, mockUser);
+
+        assertEquals(mockUser, userRepository.findById(1L));
+        assertNull(userRepository.findById(2L));
+    }
+
+    @Test
+    @DisplayName("Test updating a user by ID")
+    public void testUpdateUserById() {
+        when(updatedUser.getId()).thenReturn(1L);
+        userRepository.dbUsers.put(1L, mockUser);
+        userRepository.updateUserById(1L, updatedUser);
+
+        assertEquals(updatedUser, userRepository.dbUsers.get(1L));
+    }
+}
+
