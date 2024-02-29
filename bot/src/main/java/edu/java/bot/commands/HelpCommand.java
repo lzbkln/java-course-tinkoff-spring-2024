d1e2411ee3de1751b2da3e065eb5433f3476161c
lzbkl <lzbkln@mail.ru>
2024-02-29 22:43:36 +0500

    fix HelpCommand

--- bot/src/main/java/edu/java/bot/commands/HelpCommand.java
+++ bot/src/main/java/edu/java/bot/commands/HelpCommand.java
@@ -2,6 +2,8 @@ package edu.java.bot.commands;
 
 import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("/help")
@@ -9,19 +11,39 @@ public class HelpCommand implements Command {
     private static final String COMMAND = "/help";
     private static final String DESCRIPTION_OF_COMMAND = "Get list of all commands";
 
-    private static final String RESPONSE = """
-        This bot works as a single update tracking method for many resources.
-        First you need to register:
-        /start - Register user
-        All commands:
-        /track - Start link tracking
-        /untrack - Stop link tracking
-        /list - Show list of tracked links
-        /help - Get list of all commands""";
+    @Autowired
+    @Qualifier("/start")
+    private Command startCommand;
+
+    @Autowired
+    @Qualifier("/list")
+    private Command listCommand;
+
+    @Autowired
+    @Qualifier("/track")
+    private Command trackCommand;
+
+    @Autowired
+    @Qualifier("/untrack")
+    private Command untrackCommand;
 
     @Override
     public String execute(Update update) {
-        return RESPONSE;
+        return String.join(
+            "\n",
+            "This bot works as a single update tracking method for many resources.",
+            "First you need to register:",
+            formatCommandDescription(startCommand),
+            "All commands:",
+            formatCommandDescription(trackCommand),
+            formatCommandDescription(untrackCommand),
+            formatCommandDescription(listCommand),
+            formatCommandDescription(this)
+        );
+    }
+
+    private String formatCommandDescription(Command command) {
+        return String.format("%s - %s\n", command.nameCommand(), command.descriptionOfCommand());
     }
 
     @Override
