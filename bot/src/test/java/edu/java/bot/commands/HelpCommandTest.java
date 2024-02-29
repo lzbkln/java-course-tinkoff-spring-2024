2753d886a95cefc07e0fc076f68134ff14b08e5f
lzbkl <lzbkln@mail.ru>
2024-02-29 23:00:52 +0500

    fix HelpCommandtest because of working with @qualifier

--- bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
+++ bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
@@ -8,6 +8,7 @@ import edu.java.bot.BotApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,13 +22,34 @@ public class HelpCommandTest {
     private Message message;
     @Mock
     private Chat chat;
-    private final HelpCommand helpCommand = new HelpCommand();
+    @InjectMocks
+    private HelpCommand helpCommand;
+    @Mock
+    private Command startCommand;
+    @Mock
+    private Command listCommand;
+    @Mock
+    private Command trackCommand;
+    @Mock
+    private Command untrackCommand;
 
     @BeforeEach
     public void setUp() {
         Mockito.when(update.message()).thenReturn(message);
         Mockito.when(message.chat()).thenReturn(chat);
         Mockito.when(chat.id()).thenReturn(1L);
+
+        Mockito.when(startCommand.nameCommand()).thenReturn("/start");
+        Mockito.when(startCommand.descriptionOfCommand()).thenReturn("Register user");
+
+        Mockito.when(listCommand.nameCommand()).thenReturn("/list");
+        Mockito.when(listCommand.descriptionOfCommand()).thenReturn("Show list of tracked links");
+
+        Mockito.when(trackCommand.nameCommand()).thenReturn("/track");
+        Mockito.when(trackCommand.descriptionOfCommand()).thenReturn("Start link tracking");
+
+        Mockito.when(untrackCommand.nameCommand()).thenReturn("/untrack");
+        Mockito.when(untrackCommand.descriptionOfCommand()).thenReturn("Stop link tracking");
     }
 
     @Test
@@ -37,11 +59,16 @@ public class HelpCommandTest {
             This bot works as a single update tracking method for many resources.
             First you need to register:
             /start - Register user
+
             All commands:
             /track - Start link tracking
+
             /untrack - Stop link tracking
+
             /list - Show list of tracked links
-            /help - Get list of all commands""";
+
+            /help - Get list of all commands
+            """;
 
         assertEquals(expectedResponse, helpCommand.execute(update));
     }
