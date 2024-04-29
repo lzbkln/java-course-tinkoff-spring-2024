3af009b00b0505cfb0048345990f85a10fb530f3
lzbkl <lzbkln@mail.ru>
2024-04-29 17:31:59 +0500

    added new metric - count of messages processed

---
+++ bot/src/main/java/edu/java/bot/configuration/MicrometerConfig.java
@@ -0,0 +1,20 @@
+package edu.java.bot.configuration;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MicrometerConfig {
+
+    private final MeterRegistry meterRegistry;
+    private final ApplicationConfig applicationConfig;
+
+    @Bean
+    public Counter processedLinkUpdatesMetric() {
+        return meterRegistry.counter(applicationConfig.nameOfMetric());
+    }
+}
